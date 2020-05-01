963d07b7ef1bd930a09c1bd3c635ae435798d55d
Helder Rodrigues <a11027@alunos.ipca.pt>
2020-05-01 15:15:57 +0100

    Added partial view

---
+++ StorageInstruments/Pages/Instruments/_Summary.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StorageInstruments.Pages.Instruments
+{
+    public class _SummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
