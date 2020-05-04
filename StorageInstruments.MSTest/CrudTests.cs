14df574ef182736f53eae539427ca6f28acae72b
Helder Rodrigues <a11027@alunos.ipca.pt>
2020-05-04 16:59:55 +0100

    Added Test Initialize and started configuring mock and fixture

--- StorageInstruments.MSTest/CrudTests.cs
+++ StorageInstruments.MSTest/CrudTests.cs
@@ -11,21 +11,44 @@ namespace StorageInstruments.MSTest
     [TestClass]
     public class CrudTests
     {
+        Fixture fixture;
+        Instrument instrument;
+        Mock<IInstrumentRepository> mapMock;
+
+        [TestInitialize]
+        public void Starter()
+        {
+            // Fixture setup
+            fixture = new Fixture();
+
+            instrument = fixture.Freeze<Instrument>();
+
+            mapMock = new Mock<IInstrumentRepository>();
+
+            fixture.Register(() => mapMock.Object);
+        }
         [TestMethod]
         public void TestGetById()
         {
-            // Fixture setup
-            var fixture = new Fixture();
-
-            var instrument = fixture.Freeze<Instrument>();
-
-            var mapMock = new Mock<IInstrumentRepository>();
-            fixture.Register(() => mapMock.Object);
-
-            var aux = mapMock.Object.GetById(instrument.Id);
+            var aux = mapMock.Setup(x => x.GetById(instrument.Id)).Returns<Instrument>(x => x);
 
             Assert.IsNotNull(aux);
+        }
 
+        [TestMethod]
+        public void TestCreateInstrument()
+        {
+            Instrument fakeInstrument = new Instrument()
+            {
+                Location = LocationType.Home,
+                Name = "Test",
+                owner = "Test",
+                Type = InstrumentType.Percurssion
+            };
+
+            var aux = mapMock.Setup(x => x.Add(fakeInstrument));
+            
+            //Assert.AreEqual(aux, mapMock.Setup(x => x.GetById(aux.ob)
         }
     }
 }
