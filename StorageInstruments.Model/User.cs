9ddcc217e9ddf29b2cd68d321ae9250af9f56494
Helder Rodrigues <a11027@alunos.ipca.pt>
2020-06-02 10:52:11 +0100

    Added login classes

---
+++ StorageInstruments.Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace StorageInstruments.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(85)]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public virtual UserType UserType { get; set; }
+    }
+}
