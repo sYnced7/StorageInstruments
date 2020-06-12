810f54b4afd774d3a4daea3d7b207ebca9a82559
Helder Rodrigues <a11027@alunos.ipca.pt>
2020-06-12 12:54:36 +0100

    Added data transfer object project & created classes

---
+++ services/StorageInstruments.DTO/InstrumentDto.cs
@@ -0,0 +1,13 @@
+﻿using StorageInstruments.Model;
+
+namespace StorageInstruments.DTO
+{
+    public class InstrumentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public LocationType Location { get; set; }
+        public string owner { get; set; }
+        public InstrumentType Type { get; set; }
+    }
+}
---
+++ services/StorageInstruments.DTO/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using StorageInstruments.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageInstruments.DTO
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public virtual UserType UserType { get; set; }
+    }
+}
